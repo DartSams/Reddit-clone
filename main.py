0840c5e4ee2dbf9d66fa94bc279ef6fccba7b917
dartsams <dartagnansams1@icloud.com>
2023-01-31 02:25:12 -0500

    started adding sockets div for creating post

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask,render_template
 from dotenv import load_dotenv
+from flask_socketio import SocketIO
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 
 
@@ -10,7 +13,11 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-
+@socketio.on("like_post")
+def like_post(data):
+    print(data)
+    
 if __name__ == "__main__":
-    app.run(port=8000,debug=True)
+    # app.run(port=8000,debug=True)
+    socketio.run(app,port=8000,debug=True)
 
--- static/js/home.js
+++ static/js/home.js
@@ -1,3 +1,9 @@
+const socket = io();
+socket.on('connect', function() {
+    console.log("socket connected")
+    socket.emit('my event', {data: 'I\'m connected!'});
+});
+
 let logoButton = document.querySelector("#image-logo");
 logoButton.addEventListener("click",function(){
     console.log("Time to go home.")
@@ -36,43 +42,82 @@ registerButton.addEventListener("click",function(){
     console.log("Time to register.")
 });
 
+function getPostData(){
+    console.log("getting post data.")
+    let clickedPost = document.querySelector(".post-card-container")
+    console.log(clickedPost.getAttribute("value"))
+}
+
 let postLink = document.querySelector("#post-body");
 postLink.addEventListener("click",function(){
     console.log("Going to post page.")
+    getPostData()
 })
 
 let likePostButton = document.querySelector("#like-post");
 likePostButton.addEventListener("click",function(){
     let postValue = likePostButton.getAttribute("value")
-    console.log("liking post")
+    console.log("liking post.")
     console.log(postValue)
+    if (likePostButton.src.includes("dark") === false) { // if the image src doesnt have dark in it then it will change that src to the dark image version
+        likePostButton.src = "static/assets/like-dark.png"
+    } else { // if the image src does have dark in it then it will change the src to the light image version
+        likePostButton.src = "static/assets/like-light.png"
+    } // when liking a post changes the image to a dark to show it has been liked 
+    
+    socket.emit("like_post",{"post":0,"posted_by":"Dartsams"})
 });
 
 let dislikePostButton = document.querySelector("#dislike-post");
 dislikePostButton.addEventListener("click",function(){
     let postValue = dislikePostButton.getAttribute("value")
-    console.log("Disliking post")
+    console.log("Disliking post.")
     console.log(postValue)
+    if (dislikePostButton.src.includes("dark") === false) { // if the image src doesnt have dark in it then it will change that src to the dark image version
+        dislikePostButton.src = "static/assets/dislike-dark.png"
+    } else { // if the image src does have dark in it then it will change the src to the light image version
+        dislikePostButton.src = "static/assets/dislike-light.png"
+    } // when dislike a post changes the image to a dark to show it has been disliked 
 });
 
 let subRedditLink = document.querySelector("#subreddit");
 subRedditLink.addEventListener("click",function(){
     let subredditLinkName = subRedditLink.getAttribute("value")
-    console.log("Going to subreddit")
+    console.log("Going to subreddit.")
     console.log(subredditLinkName)
 });
 
 let posterLink = document.querySelector("#posted-by");
 posterLink.addEventListener("click",function(){
     let posterLinkName = posterLink.getAttribute("value")
-    console.log("Going to post creator page")
+    console.log("Going to post creator page.")
     console.log(posterLinkName)
 });
 
 let subredditJoinButton = document.querySelector("#join-subreddit");
 subredditJoinButton.addEventListener("click",function(){
     let subredditJoinButton = subRedditLink.getAttribute("value")
-    console.log("joining subreddit")
+    console.log("joining subreddit.")
     console.log(subredditJoinButton)
 });
 
+let postCommentButton = document.querySelector("#comments");
+postCommentButton.addEventListener("click",function(){
+    console.log("Checking post comments.")
+});
+
+let postShareButton = document.querySelector("#share");
+postShareButton.addEventListener("click",function(){
+    console.log("Sharing post.")
+});
+
+let postSaveButton = document.querySelector("#save");
+postSaveButton.addEventListener("click",function(){
+    console.log("Saving post.")
+    if (postSaveButton.firstChild.src.includes("dark") === false) { // if the image src doesnt have dark in it then it will change that src to the dark image version
+        postSaveButton.firstChild.src = "static/assets/save-dark.png"
+    } else { // if the image src does have dark in it then it will change the src to the light image version
+        postSaveButton.firstChild.src = "static/assets/save-light.png"
+    } // when saving a post changes the image to a dark to show it has been saved 
+    
+})
